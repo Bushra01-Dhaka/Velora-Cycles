f4611d50f57fdc537e87afa5d1e074678e420aa4
Bushra01-Dhaka <nothingg192@gmail.com>
2026-06-25 13:13:10 +0600

    Created Features Component also make it responsive

---
+++ src/Components/Features.jsx
@@ -0,0 +1,82 @@
+
+
+const Features = () => {
+  return (
+    <div className="flex justify-center font-outfits bg-success text-info min-h-screen py-20 px-10 lg:px-20">
+        <div>
+              <div className="text-center ">
+                      <h2 className="text-4xl text-primary lg:text-5xl font-bold font-outfit">
+                        Why Choose Velora?
+                      </h2>
+                      <p className="py-4 lg:text-lg lg:max-w-[70%] mx-auto">Engineered with precision and built for durability, our bicycles combine performance, comfort, and innovation for every journey.</p>
+              </div>
+
+              <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 p-4 lg:p-10">
+                    <div className="relative  group overflow-hidden rounded-xl hover:shadow-md shadow-primary ">
+                        <div className="">
+                            <img src="https://images.unsplash.com/photo-1722255988570-4ef0c7ecd6e2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTh8fGJpY3ljbGUlMjBsaWdodHdlaWdodCUyMGZyYW1lfGVufDB8fDB8fHww" className="h-[400px] w-full lg:w-[320px]  object-cover transition-transform duration-500 group-hover:scale-110 cursor-pointer" alt="" />
+                        </div>
+                        <div className="absolute lg:h-[150px] bottom-0 p-4  lg:w-[320px] w-full  bg-black/70">
+                            <h3 className="text-2xl text-info font-semibold">Lightweight Frames</h3>
+                            <p className="py-2 w-full lg:w-[340px] text-accent">Built with premium aluminum and carbon fiber materials for speed and agility.</p>
+                        </div>
+                    </div>
+
+                    <div className="relative  group overflow-hidden rounded-xl hover:shadow-md shadow-primary ">
+                        <div className="">
+                            <img src="https://images.unsplash.com/photo-1532298229144-0ec0c57515c7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y3ljbGluZyUyMGZyYW1lfGVufDB8fDB8fHww" className="h-[400px] w-full lg:w-[320px]  object-cover transition-transform duration-500 group-hover:scale-110 cursor-pointer" alt="" />
+                        </div>
+                        <div className="absolute lg:h-[150px] bottom-0 p-4  lg:w-[320px] w-full  bg-black/70">
+                            <h3 className="text-2xl text-info font-semibold">Advanced Suspension</h3>
+                            <p className="py-2 w-full lg:w-[340px] text-accent">Smooth rides across rough terrains with industry-leading suspension systems.</p>
+                        </div>
+                    </div>
+
+                    <div className="relative  group overflow-hidden rounded-xl hover:shadow-md shadow-primary ">
+                        <div className="">
+                            <img src="https://plus.unsplash.com/premium_photo-1726863054606-b13172dbaca1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGJpY3ljbGUlMjBicmVha3xlbnwwfHwwfHx8MA%3D%3D" className="h-[400px] w-full lg:w-[320px]  object-cover transition-transform duration-500 group-hover:scale-110 cursor-pointer" alt="" />
+                        </div>
+                        <div className="absolute lg:h-[150px] bottom-0 p-4  lg:w-[320px] w-full  bg-black/70">
+                            <h3 className="text-2xl text-info font-semibold">Safety First</h3>
+                            <p className="py-2 w-full lg:w-[340px] text-accent">Reliable braking systems and enhanced visibility for confident riding.</p>
+                        </div>
+                    </div>
+
+                    <div className="relative  group overflow-hidden rounded-xl hover:shadow-md shadow-primary ">
+                        <div className="">
+                            <img src="https://images.unsplash.com/photo-1770142281745-1d2e239cc306?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzB8fGJpY3ljbGUlMjBicmVha3xlbnwwfHwwfHx8MA%3D%3D" className="h-[400px] w-full lg:w-[320px]  object-cover transition-transform duration-500 group-hover:scale-110 cursor-pointer" alt="" />
+                        </div>
+                        <div className="absolute lg:h-[150px] bottom-0 p-4  lg:w-[320px] w-full  bg-black/70">
+                            <h3 className="text-2xl text-info font-semibold">Free Delivery</h3>
+                            <p className="py-2 w-full lg:w-[340px] text-accent">Fast and secure delivery across the country.</p>
+                        </div>
+                    </div>
+
+                    <div className="relative  group overflow-hidden rounded-xl hover:shadow-md shadow-primary ">
+                        <div className="">
+                            <img src="https://images.unsplash.com/photo-1708979606249-c6f72f3a74c1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTMwfHxiaWN5Y2xlJTIwbGlnaHR3ZWlnaHQlMjBmcmFtZXxlbnwwfHwwfHx8MA%3D%3D" className="h-[400px] w-full lg:w-[320px]  object-cover transition-transform duration-500 group-hover:scale-110 cursor-pointer" alt="" />
+                        </div>
+                        <div className="absolute lg:h-[150px] bottom-0 p-4  lg:w-[320px] w-full  bg-black/70">
+                            <h3 className="text-2xl text-info font-semibold">Lifetime Frame Warranty</h3>
+                            <p className="py-2 w-full lg:w-[340px] text-accent">Quality you can trust for years to come.</p>
+                        </div>
+                    </div>
+
+                    <div className="relative  group overflow-hidden rounded-xl hover:shadow-md shadow-primary">
+                        <div className="">
+                            <img src="https://images.unsplash.com/photo-1675798226758-a2e241b376fb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGJpY3ljbGUlMjBzZXJ2aWNpbmd8ZW58MHx8MHx8fDA%3D" className="h-[400px] w-full lg:w-[320px]  object-cover transition-transform duration-500 group-hover:scale-110 cursor-pointer" alt="" />
+                        </div>
+                        <div className="absolute lg:h-[150px] bottom-0 p-4  lg:w-[320px] w-full  bg-black/70">
+                            <h3 className="text-2xl text-info font-semibold">Professional Servicing</h3>
+                            <p className="py-2 w-full lg:w-[340px] text-accent">Keep your bicycle in peak condition with expert maintenance plans.</p>
+                        </div>
+                    </div>
+
+
+              </div>
+        </div>
+    </div>
+  )
+}
+
+export default Features
